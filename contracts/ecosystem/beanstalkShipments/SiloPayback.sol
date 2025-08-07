7547a3885da183253e27d3925d36c067a37551e2
default-juice <defaultjuice@pintofarm.org>
2025-08-07 16:59:47 +0300

    silo payback with staking-like functionality

--- contracts/ecosystem/beanstalkShipments/SiloPayback.sol
+++ contracts/ecosystem/beanstalkShipments/SiloPayback.sol
@@ -22,9 +22,20 @@ contract SiloPayback is Initializable, ERC20Upgradeable, OwnableUpgradeable {
 
     /// @dev Tracks total distributed bdv tokens. After initial mint, no more tokens can be distributed.
     uint256 public totalDistributed;
+    /// @dev Tracks total received pinto from shipments.
+    uint256 public totalReceived;
+
+    /// @dev Synthetix-style reward distribution variables
+    uint256 public rewardPerTokenStored;
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
 
     /// @dev event emitted when user redeems bdv tokens for underlying pinto
     event Redeemed(address indexed user, uint256 unripeBdvAmount, uint256 underlyingPintoAmount);
+    /// @dev event emitted when user claims rewards
+    event Claimed(address indexed user, uint256 amount, uint256 rewards);
+    /// @dev event emitted when rewards are received from shipments
+    event RewardsReceived(uint256 amount, uint256 newIndex);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -55,58 +66,47 @@ contract SiloPayback is Initializable, ERC20Upgradeable, OwnableUpgradeable {
     }
 
     /**
-     * @notice Redeems a given amount of unripe bdv tokens for underlying pinto
-     * @param amount the amount of unripe bdv tokens to redeem
-     * @param recipient the address to send the underlying pinto to
-     * @param toMode the mode to send the underlying pinto in
+     * @notice Claims accumulated rewards for the caller
+     * @param recipient the address to send the rewards to
+     * @param toMode the mode to send the rewards in
      */
-    function claim(uint256 amount, address recipient, LibTransfer.To toMode) external {
-        // send the underlying pintos to the user
+    function claim(address recipient, LibTransfer.To toMode) external updateReward(msg.sender) {
+        uint256 rewardsToClaim = rewards[msg.sender];
+        require(rewardsToClaim > 0, "SiloPayback: no rewards to claim");
+        
+        rewards[msg.sender] = 0;
+        
+        // Transfer the rewards to the recipient
         pintoProtocol.transferToken(
             pinto,
             recipient,
-            pintoToRedeem,
-            LibTransfer.From.EXTERNAL, // External since pintos are sent to the contract
+            rewardsToClaim,
+            LibTransfer.From.EXTERNAL,
             toMode
         );
-        emit Claimed(msg.sender, amount, pintoToRedeem);
-    }
-
-    /// @notice update the global index of earned rewards
-    function update() public {
-        uint256 totalSupply = pinto.balanceOf(address(this));
-        if (totalSupply > 0) {
-            uint256 _balance = pinto.balanceOf(address(this));
-            if (_balance > balance) {
-                uint256 _diff = _balance - balance;
-                if (_diff > 0) {
-                    uint256 _ratio = _diff * 1e18 / totalSupply;
-                    if (_ratio > 0) {
-                      index = index + _ratio;
-                      balance = _balance;
-                    }
-                }
-            }
-        }
+        
+        emit Claimed(msg.sender, rewardsToClaim, rewardsToClaim);
     }
 
 
-    /// @notice update the index for a user
-    /// @param recipient the user to update
-    function updateFor(address recipient) public {
-        update();
-        uint256 _supplied = balanceOf(recipient);
-        if (_supplied > 0) {
-            uint256 _supplyIndex = supplyIndex[recipient];
-            supplyIndex[recipient] = index;
-            uint256 _delta = index - _supplyIndex;
-            if (_delta > 0) {
-              uint256 _share = _supplied * _delta / 1e18;
-              claimable[recipient] += _share;
-            }
-        } else {
-            supplyIndex[recipient] = index;
+    /// @notice Modifier to update rewards for an account
+    /// @param account The account to update rewards for
+    modifier updateReward(address account) {
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
         }
+        _;
+    }
+
+    /// @notice Calculate earned rewards for an account.
+    /// The pro-rata delta between the current rewardPerTokenStored and the user's userRewardPerTokenPaid
+    /// rewardPerTokenStored is the ratio of the total amount of rewards received to the total amount of tokens distributed.
+    /// Since tokens cannot be burned, this will only increase over time.
+    /// @param account The account to calculate rewards for
+    /// @return The total earned rewards
+    function earned(address account) public view returns (uint256) {
+        return ((balanceOf(account) * (rewardPerTokenStored - userRewardPerTokenPaid[account])) / 1e18) + rewards[account];
     }
 
     /// @dev override the decimals to 6 decimal places, BDV has 6 decimals
@@ -114,31 +114,72 @@ contract SiloPayback is Initializable, ERC20Upgradeable, OwnableUpgradeable {
         return 6;
     }
 
+    /// @dev view function to get the remaining amount of silo payback tokens to be distributed
     function siloRemaining() public view returns (uint256) {
-        return totalDistributed;
+        return totalDistributed - totalReceived;
     }
 
+    /**
+     * @notice Receives Bean rewards from Beanstalk shipments
+     * @dev Called by the Beanstalk protocol to distribute rewards
+     * @param amount The amount of Bean rewards received
+     */
+    function receiveRewards(uint256 amount) external {
+        require(msg.sender == address(pintoProtocol), "SiloPayback: only pinto protocol");
+        require(amount > 0, "SiloPayback: amount must be greater than 0");
+        
+        uint256 tokenTotalSupply = totalSupply();
+        if (tokenTotalSupply > 0) {
+            rewardPerTokenStored += (amount * 1e18) / tokenTotalSupply;
+            totalReceived += amount;
+        }
+        
+        emit RewardsReceived(amount, rewardPerTokenStored);
+    }
+
+    /**
+     * @notice View function to calculate pending rewards for a user
+     * @param user The address to check pending rewards for
+     * @return The amount of pending rewards
+     */
+    function pendingRewards(address user) external view returns (uint256) {
+        return earned(user);
+    }
+
+    /**
+     * @notice View function to get the total amount of rewards distributed
+     * @return The total rewards distributed since inception
+     */
+    function totalRewardsDistributed() external view returns (uint256) {
+        return (rewardPerTokenStored * totalSupply()) / 1e18;
+    }
+
+    /////////////////// Transfer Hooks ///////////////////
+
+    /// @dev pre transfer hook to update rewards for both sender and receiver
     function _beforeTokenTransfer(
         address from,
         address to,
         uint256 amount
-    ) internal override {
-        if (from != address(0) && to != address(0)) {
-            revert("SiloPayback: cannot transfer between addresses");
+    ) internal {
+        // Update rewards for both sender and receiver to prevent gaming
+        if (from != address(0)) {
+            rewards[from] = earned(from);
+            userRewardPerTokenPaid[from] = rewardPerTokenStored;
+        }
+        if (to != address(0)) {
+            rewards[to] = earned(to);
+            userRewardPerTokenPaid[to] = rewardPerTokenStored;
         }
     }
 
-    /**
-     * @dev override the transfer function to enforce claiming before transferring
-     */
+    /// @dev need to override the transfer function to update rewards
     function transfer(address to, uint256 amount) public override returns (bool) {
         _beforeTokenTransfer(msg.sender, to, amount);
         return super.transfer(to, amount);
     }
 
-    /**
-     * @dev override the transferFrom function to enforce claiming before transferring
-     */
+    /// @dev need to override the transferFrom function to update rewards
     function transferFrom(
         address from,
         address to,
