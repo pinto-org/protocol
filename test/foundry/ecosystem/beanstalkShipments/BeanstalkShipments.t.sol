f70884963c8b1f3404ca7ed61a7e1c2888861616
default-juice <defaultjuice@pintofarm.org>
2025-08-15 11:28:35 +0300

    test setup

--- test/foundry/ecosystem/beanstalkShipments/BeanstalkShipments.t.sol
+++ test/foundry/ecosystem/beanstalkShipments/BeanstalkShipments.t.sol
@@ -5,6 +5,8 @@ pragma abicoder v2;
 import {TestHelper} from "test/foundry/utils/TestHelper.sol";
 import {OperatorWhitelist} from "contracts/ecosystem/OperatorWhitelist.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {console} from "forge-std/console.sol";
+
 
 /**
  * @notice Tests that the whole shipments initialization and logic works correctly.
@@ -15,16 +17,58 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
  */
 contract BeanstalkShipmentsTest is TestHelper {
 
+    // Contracts
+    address constant SHIPMENT_PLANNER = address(0x1152691C30aAd82eB9baE7e32d662B19391e34Db);
+    address constant SILO_PAYBACK = address(0x9E449a18155D4B03C2E08A4E28b2BcAE580efC4E);
+    address constant BARN_PAYBACK = address(0x71ad4dCd54B1ee0FA450D7F389bEaFF1C8602f9b);
+
+    // Owners
+    address constant PCM = address(0x2cf82605402912C6a79078a9BBfcCf061CbfD507);
+    address constant PINTO = address(0xD1A0D188E861ed9d15773a2F3574a2e94134bA8f);
+
+    // Users
+    address farmer1 = makeAddr("farmer1");
+    address farmer2 = makeAddr("farmer2");
+    address farmer3 = makeAddr("farmer3");
+
+    // Contracts
+    ISiloPayback siloPayback = ISiloPayback(SILO_PAYBACK);
+    IBarnPayback barnPayback = IBarnPayback(BARN_PAYBACK);
+    IMockFBeanstalk bs = IMockFBeanstalk(PINTO);
+
     // we need to:
-    // - 1. recreate a mock beanstalk repayment field with a mock podline
-    // - 2. deploy the silo payback proxy
-    // - 3. deploy the barn payback proxy
-    // - 4. create a function and make sure shipments are set correctly in the initialization in ShipmentDeployer.sol
-    // - 5. get to a supply where the beanstalk shipments kick in
-    // - 6. make the system print, check distribution in each contract
-    // - 6. for each component, make sure everything is set correctly, 
-    // all tokens are distributed correctly and users can claim their rewards
+    // - Verify that all state matches the one in the json files for shipments, silo and barn payback
+    // - get to a supply where the beanstalk shipments kick in
+    // - make the system print, check distribution in each contract
+    // - for each component, make sure everything is distributed correctly
+    // - make sure that at any point all users can claim their rewards pro rata
     function setUp() public {
-        
+        // mint 1bil pinto
+        // deal(PINTO, farmer1, 1_000_000_000e6);
     }
+
+    //////////////////////// STATE VERIFICATION ////////////////////////
+    // note: get properties from json, see l2migration 
+
+    function test_shipment_state() public {
+        console.log("Bs Field data:");
+        console.logBytes(abi.encode(1));
+
+        console.log("Silo payback:");
+        console.logBytes(abi.encode(SILO_PAYBACK));
+        console.log("Barn payback:");
+        console.logBytes(abi.encode(BARN_PAYBACK));
+    }
+
+    function test_repayment_field_state() public {}
+
+    function test_silo_payback_state() public {}
+
+    function test_barn_payback_state() public {}
+    
+
+    //////////////////////// SHIPMENT DISTRIBUTION ////////////////////////
+    // note: test distribution at the edge, 
+    // note: test when a payback is done
+    // note: test that all users can claim their rewards at any point
 }
